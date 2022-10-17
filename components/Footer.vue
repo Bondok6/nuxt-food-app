a650af65c2e989b26c3700df0f7040ce2eebad59
Bondok6 <kirolousbondok6@gmail.com>
2022-10-17 14:40:06 +0200

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <p>
       Inspired by Sarah Drasner for her course 'Building Applications with Vue,
-      Nuxt' on Frontend Masters
+      Nuxt' on Frontend Masters &copy; kyrillos hany 2022.
     </p>
   </footer>
 </template>
