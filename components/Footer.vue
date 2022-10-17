bafd5505f17e80777a2ae5498e9207c25e3a9603
Bondok6 <kirolousbondok6@gmail.com>
2022-10-17 14:39:16 +0200

    update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
   <footer>
-    <p>&copy; CopyRight Kyrillos Hany 2022</p>
+    <p>
+      Inspired by Sarah Drasner for her course 'Building Applications with Vue,
+      Nuxt' on Frontend Masters
+    </p>
   </footer>
 </template>
